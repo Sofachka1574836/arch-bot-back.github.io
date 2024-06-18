3f9ea57f6f9403afbce3baae844304414ea4e5c7
Sofachka1574836 <sofiamytareva@yandex.ru>
2024-06-18 17:05:57 +0300

    second commit

--- accounts/views.py
+++ accounts/views.py
@@ -104,3 +104,23 @@ from .models import Profile
 class ProfileView(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Profile
+from .serializers import ProfileSerializer
+
+@api_view(['GET', 'POST'])
+def profile_list(request):
+    if request.method == 'GET':
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
